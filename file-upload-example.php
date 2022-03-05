66d8edb1c306a10a20c65c64215d5c05d3ec7740
RAPRAP <70806823+raprap-info@users.noreply.github.com>
2022-03-05 09:31:31 +0800

    chagne

--- file-upload-example.php
+++ file-upload-example.php
@@ -1,26 +1,35 @@
 <?php
 /**
-* Plugin Name: Form Example Process Submit
+* Theme Name: Form Example Process Submit
 * Description: Form Example Process Submit Description.
 **/
-function form_include()
-{
+  
+  function form_include(){
     /* create a variable to hold the html information */
-    $content '; /* creates a string variable */
+   
+     
+    
+    /* creates a string variable */
+    
+     $content .= '';
+    
     /* open the form tag */
-    $content .= '<form method="post" action="' plugin_dir_url(_FILE_) 'process/">'
+   
+    
+    $content .= '<form method="post" action="' .plugin_dir_url(_FILE_) . 'fprocess/">';
     $content .= '<input type="text" name="your_name" placeholder="Your Full Name" />';
     $content .= '<input type="email" name="your_email" placeholder="Enter Your Email">';
-    $content .= '<input type="file" name="my_uniqų" />';
+    $content .= '<input type="file" name="my_unique_file" />';
     $content .= '<input type="submit" name="form_exaple_contact_submit" value="SUBMIT">';
     
     /* close the form tag */
     
-    $content '</form>';
+    $content .= '</form>';
+    
     return $content;
     
     }
+    
 add_shortcode('form_example','form_include');
-
 
 ?>
